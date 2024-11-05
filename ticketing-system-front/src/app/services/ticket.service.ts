11e8a3e437447dfb5f09cfefab457b1a27d6e780
Michal Tomaszewski <groobytt@gmail.com>
2024-11-05 17:05:48 +0100

    fix/ fix bug

--- ticketing-system-front/src/app/services/ticket.service.ts
+++ ticketing-system-front/src/app/services/ticket.service.ts
@@ -13,13 +13,17 @@ export class TicketService {
   }
 
   searchTickets(searchCriteria: SearchTicketCriteria, pageNumber: number) {
+    searchCriteria.createdAfter = searchCriteria.createdAfter.trim()
+    searchCriteria.createdBefore = searchCriteria.createdBefore.trim()
+
     return this.http.get<TicketResponseDto>(this.ticketUrl +
-      `/search?customerId=${searchCriteria.customerId}&ticketId=${searchCriteria.ticketId}
-      &customerPhone=${searchCriteria.customerPhone}&customerEmail=${searchCriteria.customerEmail}
-      &content=${searchCriteria.content}&isOpen=${searchCriteria.isOpen}&channel=${searchCriteria.channel}
-      &categoryId=${searchCriteria.categoryId}&subcategoryId=${searchCriteria.subcategoryId}
-      &priority=${searchCriteria.priority}&openedById=${searchCriteria.openedById}
-      &closedById=${searchCriteria.closedById}&createdAfter=${searchCriteria.createdAfter}
-      &createdBefore=${searchCriteria.createdBefore}&pageSize=10&pageNumber=${pageNumber}`,);
+      `/search?customerId=${searchCriteria.customerId}`+
+      `&ticketId=${searchCriteria.ticketId}&customerPhone=${searchCriteria.customerPhone}`+
+      `&customerEmail=${searchCriteria.customerEmail}&content=${searchCriteria.content}`+
+      `&isOpen=${searchCriteria.isOpen}&channel=${searchCriteria.channel}&categoryId=${searchCriteria.categoryId}`+
+      `&subcategoryId=${searchCriteria.subcategoryId}&priority=${searchCriteria.priority}`+
+      `&openedById=${searchCriteria.openedById}&closedById=${searchCriteria.closedById}`+
+      `&createdAfter=${searchCriteria.createdAfter}&createdBefore=${searchCriteria.createdBefore}`+
+      `&pageSize=10&pageNumber=${pageNumber}`);
   }
 }
